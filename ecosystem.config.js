ae3d8206a528d10677169a64ab94df2e682d2bbb
Shaun Lewin <lewinks@yahoo.com>
2021-05-31 11:13:31 -0400

    adds pm2 deployment config

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    name: 'hello-world',
+    version: '0.9.0',
+    script: 'index.js',
+    watch: 'false'
+  }],
+  deploy : {
+    qa : {
+      user : 'pi',
+      host : '192.168.0.19',
+      ref  : 'origin/master',
+      repo : 'git@github.com:slewin419/nodejs-hello-world.git',
+      path : '/var/www/html/nodejs-hello-world',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env qa',
+      'pre-setup': ''
+    }
+  }
+};
